b7a80741dbaed43cebfb35954ee9411e3151cced
ilyas <ilyasers@gmail.com>
2021-11-08 01:50:08 +0300

    feat: add arguments for cli

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,12 +2,18 @@
 
 import fs from 'fs';
 import { program } from 'commander';
+import genDiff from '../src/index.js';
 
 const packageInfo = JSON.parse(fs.readFileSync('./package.json').toString());
 
 program
   .version(packageInfo.version)
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .argument('<file1>')
+  .argument('<file2>')
+  .action((file1, file2) => {
+    console.log(genDiff(file1, file2));
+  });
 
 program.parse();
