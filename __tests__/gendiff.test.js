8ca0f1624f9735c94a46b4342dcff460156618b5
ilyas <ilyasers@gmail.com>
2021-11-08 01:49:52 +0300

    test: add basic test

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import genDiff from '../src';
+
+test('diff of 2 json files', () => {
+  const filePath1 = './__fixtures__/file1.json';
+  const filePath2 = './__fixtures__/file2.json';
+
+  const diff = genDiff(filePath1, filePath2);
+
+  const expectedDiff = fs.readFileSync('./__fixtures__/diff.1.2.txt').toString();
+
+  expect(diff).toBe(expectedDiff);
+});
