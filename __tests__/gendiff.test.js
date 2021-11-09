f8292c25854d03f66a2516c808e18c3a74372c78
ilyas <ilyasers@gmail.com>
2021-11-09 23:52:24 +0300

    test: add test for yaml format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -11,3 +11,14 @@ test('diff of 2 json files', () => {
 
   expect(diff).toBe(expectedDiff);
 });
+
+test('diff of 2 yaml files', () => {
+  const filePath1 = './__fixtures__/file1.yml';
+  const filePath2 = './__fixtures__/file2.yml';
+
+  const diff = genDiff(filePath1, filePath2);
+
+  const expectedDiff = fs.readFileSync('./__fixtures__/diff.1.2.txt').toString();
+
+  expect(diff).toBe(expectedDiff);
+});
