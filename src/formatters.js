62e506c3b9e652e22bbe5a4e3443bf48ab41afd5
ilyas <ilyasers@gmail.com>
2021-11-12 09:57:10 +0300

    fix: separate formatter from diff generator

---
+++ src/formatters.js
@@ -0,0 +1,51 @@
+const stylish = (diff) => {
+  const diffSymbols = {
+    same: ' ',
+    removed: '-',
+    added: '+',
+  };
+
+  const space = ' ';
+
+  const getIndent = (itemType, depth) => {
+    const diffSymbol = diffSymbols[itemType];
+    return space.repeat(4 * (depth - 1)) + space.repeat(2) + diffSymbol + space;
+  };
+
+  const wrapWithBrackets = (value, depth) => ['{', value, `${space.repeat(4 * (depth - 1))}}`].join('\n');
+
+  const stringify = (obj, depth) => {
+    const isPrimitive = !(obj instanceof Object);
+    if (isPrimitive) return obj;
+
+    const lines = Object.keys(obj).flatMap((key) => {
+      const value = obj[key];
+      return `${getIndent('same', depth)}${key}: ${stringify(value, depth + 1)}`;
+    });
+
+    return wrapWithBrackets(lines.join('\n'), depth);
+  };
+
+  const iter = (value, depth) => {
+    const lines = value.map((item) => {
+      if (item.children) {
+        const nestedValue = iter(item.children, depth + 1);
+        return `${getIndent(item.type, depth)}${item.key}: ${nestedValue}`;
+      }
+
+      return `${getIndent(item.type, depth)}${item.key}: ${stringify(item.value, depth + 1)}`;
+    });
+
+    return wrapWithBrackets(lines.join('\n'), depth);
+  };
+
+  return iter(diff, 1);
+};
+
+const getFormatter = (type = 'json') => {
+  if (type === 'json') return stylish;
+
+  throw new Error('Unknown formatter type');
+};
+
+export default getFormatter;
--- src/index.js
+++ src/index.js
@@ -2,27 +2,10 @@ import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
 import getParser from './parsers.js';
+import getFormatter from './formatters.js';
 
 const getValuesDiff = (oldObj, newObj) => {
-  const space = ' ';
-  const indent = space.repeat(4);
-
   const isPrimitive = (value) => !(value instanceof Object);
-
-  const getIndent = (depth, symbol = ' ') => space.repeat(4 * (depth - 1)) + space.repeat(2) + symbol + space;
-
-  const getKVPair = (key, value) => `${key}: ${value}`;
-
-  const stringify = (value, depth) => {
-    if (isPrimitive(value)) return value;
-    const obj = value;
-    const keys = Object.keys(obj).sort();
-    const lines = keys.map((key) => {
-      const nestedValue = stringify(obj[key], depth + 1);
-      return getIndent(depth) + getKVPair(key, nestedValue);
-    });
-    return ['{', ...lines, `${indent.repeat(depth - 1)}}`].join('\n');
-  };
 
   const iter = (obj1, obj2, depth) => {
     const keys1 = Object.keys(obj1);
@@ -30,25 +13,26 @@ const getValuesDiff = (oldObj, newObj) => {
     const allKeys = [].concat(keys1, keys2);
     const uniqKeys = _.uniq(allKeys).sort();
 
-    const diffLines = uniqKeys.flatMap((key) => {
+    return uniqKeys.flatMap((key) => {
       const v1 = obj1[key];
       const v2 = obj2[key];
-      const isBothPrimitive = isPrimitive(v1) && isPrimitive(v2);
+
+      const isEqual = isPrimitive(v1) && isPrimitive(v2) && v1 === v2;
+      if (isEqual) {
+        return { key, value: v1, type: 'same' };
+      }
+
       const isBothObjects = !isPrimitive(v1) && !isPrimitive(v2);
-
-      if (isBothPrimitive && v1 === v2) { return getIndent(depth) + getKVPair(key, v1); }
-
-      if (!isBothObjects) {
+      const isChanged = !isBothObjects;
+      if (isChanged) {
         return [
-          _.has(obj1, key) && getIndent(depth, '-') + getKVPair(key, stringify(v1, depth + 1)),
-          _.has(obj2, key) && getIndent(depth, '+') + getKVPair(key, stringify(v2, depth + 1)),
+          _.has(obj1, key) && { key, value: v1, type: 'removed' },
+          _.has(obj2, key) && { key, value: v2, type: 'added' },
         ].filter(Boolean);
       }
 
-      return getIndent(depth) + getKVPair(key, iter(v1, v2, depth + 1));
+      return { key, children: iter(v1, v2, depth + 1), type: 'same' };
     });
-
-    return ['{', ...diffLines, `${indent.repeat(depth - 1)}}`].join('\n');
   };
 
   return iter(oldObj, newObj, 1);
@@ -59,13 +43,15 @@ const readFile = (filePath) => {
   return fs.readFileSync(fullPath).toString();
 };
 
-const genDiff = (oldFilePath, newFilePath) => {
+const genDiff = (oldFilePath, newFilePath, formatterType) => {
   const parse = getParser(path.extname(oldFilePath));
 
   const oldObj = parse(readFile(oldFilePath));
   const newObj = parse(readFile(newFilePath));
   const diff = getValuesDiff(oldObj, newObj);
-  return diff;
+
+  const format = getFormatter(formatterType);
+  return format(diff);
 };
 
 export default genDiff;
